3a1cc0dbd9de1c4f8410e95350d20d998279e3a5
Bernard Mizzi <bernard.mizzi@gmail.com>
2023-09-17 12:37:16 +0200

    Created dot files from the generated trees that can be converted to images

---
+++ Dot_files/dt.bat
@@ -0,0 +1,12 @@
+call dot -Tpng tree_1.dot -o tree_1.png -Gdpi=600
+call dot -Tpng tree_2.dot -o tree_2.png -Gdpi=600
+call dot -Tpng tree_3.dot -o tree_3.png -Gdpi=600
+call dot -Tpng tree_4.dot -o tree_4.png -Gdpi=600
+call dot -Tpng tree_5.dot -o tree_5.png -Gdpi=600
+call dot -Tpng tree_6.dot -o tree_6.png -Gdpi=600
+call dot -Tpng tree_7.dot -o tree_7.png -Gdpi=600
+call dot -Tpng tree_8.dot -o tree_8.png -Gdpi=600
+call dot -Tpng tree_9.dot -o tree_9.png -Gdpi=600
+call dot -Tpng tree_10.dot -o tree_10.png -Gdpi=600
+call dot -Tpng tree_11.dot -o tree_10.png -Gdpi=600
+pause
